4c213cf0e48239d441bad7fce351823121880162
yaboimarf <mfolst2006@gmail.com>
2025-09-15 13:48:39 +0200

    ik kan eindelijk mn stuff pushen

---
+++ Assets/Scripts/Player/Abillities.cs
@@ -0,0 +1,13 @@
+public enum Abilities
+{
+    None,
+    AbilityShield,
+    AbilityNet,
+    AbilityCrossbow,
+    AbilityDash,
+    AbilityBersekerRage,
+    AbilityThrow,
+    AbilitySiphon,
+    AbilitySteadyStance,
+    AbilityMarathonRunner,
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayerHit(int Damage)
+    {
+       
+    }
+}
