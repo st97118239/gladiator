8976927fde23b6b2ebaeff2250d49f7d8793020f
joas <97118239@st.deltion.nl>
2025-09-17 14:03:33 +0200

    Attacken heeft nu ook een direction en cooldown
    Je attackt nu de kant op waar je muis staat
    Je moet nu even wachten totdat je weer kan slaan
    Speed variable verplaatst naar player movement want het is voor de movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,34 +8,50 @@ public class Player : MonoBehaviour
 {
     public InputAction meleeAction;
     public int health;
-    public float speed;
     public float movementSpeed;
     public bool isDead;
     public int meleeDamage;
+    public float meleeAtkSpeed;
 
     [SerializeField] private Transform meleeWeaponHitbox;
+    [SerializeField] private Transform aimTransform;
+    [SerializeField] private float meleeHitboxDistanceFromPlayer;
     [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private ContactFilter2D filter;
 
     [SerializeField] private Slider hpSlider;
+    [SerializeField] private Camera cam;
+
+    private bool canAttack;
 
     private void Start()
     {
         meleeAction.Enable();
         hpSlider.maxValue = health;
         hpSlider.value = health;
+        meleeWeaponHitbox.position += Vector3.up * meleeHitboxDistanceFromPlayer;
+        canAttack = true;
     }
 
     private void Update()
     {
-        speed = movementSpeed * Time.deltaTime;
-
         meleeAction.started += ctx => { MeleeAttack(); };
     }
 
     private void MeleeAttack()
     {
+        if (!canAttack) return;
+
         Debug.Log("LMB");
+
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePos.x, mousePos.y, 0);
+
+        Vector3 aimDir = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+        angle -= 90;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
 
         List<Collider2D> hitColliders = new();
         Physics2D.OverlapBox(meleeWeaponHitbox.position, meleeWeaponHitbox.localScale / 2, meleeWeaponHitbox.rotation.z, filter, hitColliders);
@@ -46,14 +63,27 @@ public class Player : MonoBehaviour
             hitColliders[i].GetComponent<EnemyController>().Hit(meleeDamage);
             i++;
         }
+
+        StartCoroutine(AttackDelay());
+    }
+
+    private IEnumerator AttackDelay()
+    {
+        canAttack = false;
+
+        WaitForSeconds wait = new(meleeAtkSpeed);
+
+        yield return wait;
+
+        canAttack = true;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        if (!Application.isPlaying) return;
 
-        if (Application.isPlaying)
-            Gizmos.DrawWireCube(meleeWeaponHitbox.position, meleeWeaponHitbox.localScale);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(cam.ScreenToWorldPoint(Input.mousePosition), transform.position);
     }
 
     public void PlayerHit(int damage)
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,28 +1,31 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Player player;
     public Rigidbody2D rb2d;
-    void Update()
+
+    private float speed;
+
+    private void Update()
     {
+        speed = player.movementSpeed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.W))
         {
-            rb2d.AddForce(transform.up * player.speed, ForceMode2D.Force);
+            rb2d.AddForce(transform.up * speed, ForceMode2D.Force);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb2d.AddForce(transform.right * player.speed * -1, ForceMode2D.Force);
+            rb2d.AddForce(transform.right * (speed * -1), ForceMode2D.Force);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb2d.AddForce(transform.up * player.speed * -1, ForceMode2D.Force);
+            rb2d.AddForce(transform.up * (speed * -1), ForceMode2D.Force);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb2d.AddForce(transform.right * player.speed, ForceMode2D.Force);
+            rb2d.AddForce(transform.right * speed, ForceMode2D.Force);
         }
     }
 }
