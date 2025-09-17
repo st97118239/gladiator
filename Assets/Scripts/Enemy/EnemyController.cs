e610017051b32cb78b42b669c56bc63b7ae8f134
joas <97118239@st.deltion.nl>
2025-09-17 10:32:57 +0200

    Player attacking toegevoegt
    Player kan nu attacken
    Het spel gaat nu ook echt door naar de volgende wave

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -9,11 +9,23 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private string spritePath;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
+    [SerializeField] private EnemyStateMachine enemyStateMachine;
+
     public void Load(Enemy givenEnemy, EnemyManager givenManager)
     {
         enemyManager = givenManager;
         enemy = givenEnemy;
         health = enemy.health;
         spriteRenderer.sprite = Resources.Load<Sprite>(spritePath + enemy.enemyType);
+        enemyStateMachine.Load();
+    }
+
+    public void Hit(int damage)
+    {
+        health -= damage;
+
+        if (health > 0) return;
+        Debug.Log("Killed enemy.");
+        enemyManager.CheckIfEnd(this);
     }
 }
--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -1,26 +1,90 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    // When an enemy dies, it'll check if all enemies are dead. If so, start next wave. If no next waves, you win
+
     public Level level;
     public int currentWave;
 
     public EnemyManager enemyManger;
 
+    [SerializeField] private int levelStartDelay;
+    [SerializeField] private int waveStartDelay;
+    [SerializeField] private GameObject countdownBox;
+    [SerializeField] private TMP_Text countdownText;
+    [SerializeField] private GameObject gameFinishedPanel;
+
+    private float countdown;
+
     private void Start()
     {
+        gameFinishedPanel.SetActive(false);
         StartLevel();
     }
 
     private void StartLevel()
     {
         enemyManger.EmptyEnemySpawn();
+        StartCoroutine(LevelCountdown());
+    }
+
+    private IEnumerator LevelCountdown()
+    {
+        WaitForSeconds wait1Second = new(1);
+
+        countdownBox.SetActive(true);
+
+        while (levelStartDelay > 0)
+        {
+            countdownText.text = levelStartDelay.ToString();
+
+            yield return wait1Second;
+
+            levelStartDelay -= 1;
+        }
+
+        countdownBox.SetActive(false);
+
         StartWave();
     }
 
-    public void StartWave()
+    private void StartWave()
     {
         enemyManger.SpawnEnemy(level.waves[currentWave]);
         currentWave++;
+    }
+
+    public void WaveEnd()
+    {
+        Debug.Log("Wave ended.");
+
+        if (level.waves.Count > currentWave)
+            StartCoroutine(NextWave());
+        else
+        {
+            Debug.Log("Game finished.");
+            gameFinishedPanel.SetActive(true);
+        }
+    }
+
+    private IEnumerator NextWave()
+    {
+        Debug.Log("Countdown for new wave.");
+
+        WaitForSeconds wait1Second = new(1);
+
+        countdown = waveStartDelay;
+
+        while (countdown > 0)
+        {
+            yield return wait1Second;
+
+            countdown -= 1;
+        }
+
+        StartWave();
     }
 }
